7b27ad20e03bf3bacd1215973cd1731f90dde7a2
Tracy <junlantracyli@gmail.com>
2023-12-23 16:38:05 -0800

    Pokemon API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
+      .then(response => {
+        // this will return the data from the response object
+        //    in a json format
+        return response.json()
+      })
+      .then(response => {
+        // store the json converted data in state so it can be displayed
+        setPokemon(response.results)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    
+    // no clean up statement is required since we are not using resources
+    //    that will continue to run
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ width: "200px", margin: "auto" }}>
+      <ul>
+        {
+          pokemon.map((pokemonCharac, index) => {
+            return <li key={index}>{pokemonCharac.name}</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
